37ebe3f81171a4679014b12e5ff03cf848280e69
Nikos Vourdas <nickvourd97@gmail.com>
2023-08-08 15:13:34 +0300

    Added ascii

--- Supernova.go
+++ Supernova.go
@@ -5,7 +5,32 @@ import (
 	"fmt"
 )
 
+// global variables
+var (
+	__version__ = "1.0.0"
+	__license__ = "MIT"
+	__author__  = "@nickvourd"
+	__github__  = "https://github.com/nickvourd/Supernova"
+)
+
+var __ascii__ = `
+
+███████╗██╗   ██╗██████╗ ███████╗██████╗ ███╗   ██╗ ██████╗ ██╗   ██╗ █████╗ 
+██╔════╝██║   ██║██╔══██╗██╔════╝██╔══██╗████╗  ██║██╔═══██╗██║   ██║██╔══██╗
+███████╗██║   ██║██████╔╝█████╗  ██████╔╝██╔██╗ ██║██║   ██║██║   ██║███████║
+╚════██║██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗██║╚██╗██║██║   ██║╚██╗ ██╔╝██╔══██║
+███████║╚██████╔╝██║     ███████╗██║  ██║██║ ╚████║╚██████╔╝ ╚████╔╝ ██║  ██║
+╚══════╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝   ╚═══╝  ╚═╝  ╚═╝
+
+Supernova v.%s - A real fucking shellcode encryptor.
+Supernova is an open source tool licensed under %s.
+Written with <3 by %s...
+Please visit %s for more...
+
+`
+
 // main function
 func main() {
-	fmt.Println("Hello World")
+	//Print ascii
+	fmt.Printf(__ascii__, __version__, __license__, __author__, __github__)
 }
