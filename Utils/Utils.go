dbe7931995f0b23068690dbb086021dc7c1da4ec
Nikos Vourdas <nickvourd97@gmail.com>
2023-08-09 10:59:54 +0300

    Starting Encryptors

--- Utils/Utils.go
+++ Utils/Utils.go
@@ -16,6 +16,6 @@ func Version() {
 	if VerNumb >= 19.1 {
 	} else {
 		logger := log.New(os.Stderr, "[!] ", 0)
-		logger.Fatal("[!} The version of Go is to old, please update to version 1.19.1 or later...")
+		logger.Fatal("The version of Go is to old, please update to version 1.19.1 or later...")
 	}
 }
