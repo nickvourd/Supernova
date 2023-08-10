b5e1cb4af3325c37f972019f0adb3f042f522082
Nikos Vourdas <nickvourd97@gmail.com>
2023-08-10 12:38:23 +0300

    Create XOR output templates but not tested yet

--- Output/Output.go
+++ Output/Output.go
@@ -18,6 +18,36 @@ var __csharp_xor__ = `static byte[] XORDecryption(byte[] %s, byte[] key)
 	return decrypted;
 }`
 
+var __c_xor__ = `unsigned char* XORDecryption(unsigned char* %s, unsigned char* key, size_t %sLength, size_t keyLength){
+    unsigned char* decrypted = (unsigned char*)malloc(%sLength);
+    
+    for (size_t i = 0; i < %sLength; i++) {
+        decrypted[i] = %s[i] ^ key[i %% keyLength];
+    }
+
+    return decrypted;
+}`
+
+var __rust_xor__ = `fn XORDecryption(%s: &[u8], key: &[u8]) -> Vec<u8> {
+    let mut decrypted = Vec::with_capacity(%s.len());
+    let key_len = key.len();
+
+    for i in 0..%s.len() {
+        decrypted.push(%s[i] ^ key[i %% key_len]);
+    }
+
+    decrypted
+}`
+
+var __nim_xor__ = `proc XORDecryption(%s: array[byte], key: array[byte]): array[byte] =
+  var decrypted: array[byte, %s.len]
+  let keyLen = key.len
+
+  for i in 0..<%s.len:
+    decrypted[i] = %s[i] xor key[i mod keyLen]
+
+  result = decrypted`
+
 // OutputDecryption function
 func OutputDecryption(language string, variable string, encryption string, key []byte) {
 	switch strings.ToLower(encryption) {
@@ -27,6 +57,24 @@ func OutputDecryption(language string, variable string, encryption string, key [
 			fmt.Printf("[+] %s function for decryption (%s):\n\n"+__csharp_xor__+"\n\n", strings.ToUpper(language), strings.ToLower(encryption), variable, variable, variable, variable)
 			fmt.Printf("[+] Set key in main:\n\nbyte[] key = new byte[] { /* XOR key bytes here like 0xfc, 0x55 ...*/ };\n\n")
 			fmt.Printf("[+] Call function in main:\n\n"+"%s = XORDecryption(%s, key);\n\n", variable, variable)
+		case "c":
+			fmt.Printf("[+] %s function for decryption (%s):\n\n"+__c_xor__+"\n\n", strings.ToUpper(language), strings.ToLower(encryption), variable, variable, variable, variable, variable)
+			fmt.Printf("[+] Set key in main:\n\nunsigned char key[] = {/* XOR key bytes here like 0xfc, 0x55 ...*/};\n\n")
+			fmt.Printf("[+] Store key & shellcode size (in bytes) in main:\n\nsize_t %sLength = sizeof(%s);\n\nsize_t keyLength = sizeof(key);\n\n", variable, variable)
+			fmt.Printf("[+] Call function in main:\n\n"+"%s = XORDecryption(%s, key, %sLength, keyLength);\n\n", variable, variable, variable)
+		case "rust":
+			fmt.Printf("[+] %s function for decryption (%s):\n\n"+__rust_xor__+"\n\n", strings.ToUpper(language), strings.ToLower(encryption), variable, variable, variable, variable)
+			fmt.Printf("[+] Set key in main:\n\nlet key: Vec<u8> = vec![/* XOR key bytes here like 0xfc, 0x55 ...*/];\n\n")
+			fmt.Printf("[+] Call function in main:\n\n"+"%s = XORDecryption(&%s, &key);\n\n", variable, variable)
+		case "nim":
+			fmt.Printf("[+] %s function for decryption (%s):\n\n"+__nim_xor__+"\n\n", strings.ToUpper(language), strings.ToLower(encryption), variable, variable, variable, variable)
+			fmt.Printf("[+] Set key in main:\n\nvar key: array[byte] = @[/* XOR key bytes here like 0xfc, 0x55 ...*/]\n\n")
+			fmt.Printf("[+] Call function in main:\n\n"+"%s = XORDecryption(%s, key)\n\n", variable, variable)
+		}
+	case "rc4":
+		switch language {
+		case "csharp":
+			fmt.Println("Hello World")
 		}
 	}
 }
