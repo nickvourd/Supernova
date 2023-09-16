6f7b283fd9c2a16e55cb42bb8f05fdf305471b17
unknown <nickvourd97@gmail.com>
2023-09-16 14:56:18 +0300

    Fix hex output in C# ROT decryption

--- Decryptors/Decryptors.go
+++ Decryptors/Decryptors.go
@@ -17,13 +17,32 @@ namespace ROTDecryption
 	{
 		static void Main(string[] args)
 		{
-		
 			byte[] %s = new byte[%d] {%s};
 			int encryptedKey = %d;
 			byte[] decryptedPayload = DecryptROTPayload(%s, encryptedKey);
 			string payloadText = Encoding.ASCII.GetString(decryptedPayload);
-			Console.WriteLine("Decrypted Payload: " + payloadText);
+			
+			// Convert decryptedPayload to a hexadecimal string
+            StringBuilder hex = new StringBuilder(decryptedPayload.Length * 2);
+            int totalCount = decryptedPayload.Length;
+            for (int count = 0; count < totalCount; count++)
+            {
+                byte b = decryptedPayload[count];
+
+                if ((count + 1) == totalCount) // Don't append a comma for the last item
+                {
+                    hex.AppendFormat("0x{0:x2}", b);
+                }
+                else
+                {
+                    hex.AppendFormat("0x{0:x2}, ", b);
+                }
+            }
+
+            Console.WriteLine("ROT Decrypted Payload:\n");
+            Console.WriteLine($"byte[] %s = new byte[{%s.Length}] {{ {hex} }};\n\n");
 		}
+
 		static byte[] DecryptROTPayload(byte[] encryptedData, int key)
 		{
 			byte[] decrypted = new byte[encryptedData.Length];
@@ -84,7 +103,7 @@ func DecryptorsTemplates(language string, cipher string, variable string, key in
 		switch strings.ToLower(cipher) {
 		case "rot":
 			// Config dynamic variable
-			__csharp_rot__ = fmt.Sprintf(__csharp_rot__, variable, payloadSize, encryptedShellcode, key, variable)
+			__csharp_rot__ = fmt.Sprintf(__csharp_rot__, variable, payloadSize, encryptedShellcode, key, variable, variable, variable)
 
 			// Call function named SaveTamplate2File
 			SaveTamplate2File(foundFilename, __csharp_rot__, cipher)
