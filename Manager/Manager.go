b052d0bbd229d27881f544fde06ec406edf4286d
unknown <nickvourd97@gmail.com>
2024-03-27 21:29:07 +0200

    Add dev

---
+++ Manager/Manager.go
@@ -0,0 +1,56 @@
+package Manager
+
+import (
+	"Supernova/Arguments"
+	"Supernova/Converters"
+	"Supernova/Encryptors"
+	"Supernova/Output"
+	"fmt"
+	"strings"
+)
+
+// EncryptionManager function
+func EncryptionManager(Key int, Encryption string, Obfuscation string, Variable string, rawShellcode string, foundLanguage string, fileSizeFlag bool) (string, string) {
+	// Call function ValidateKeySize
+	Key = Arguments.ValidateKeySize(Key, Encryption)
+
+	// Call function named DetectEncryption
+	encryptedShellcode, encryptedLength := Encryptors.DetectEncryption(Encryption, rawShellcode, Key, foundLanguage)
+
+	// Call function named ConvertShellcode2Template
+	template := Converters.ConvertShellcode2Template(encryptedShellcode, foundLanguage, encryptedLength, Variable)
+
+	// if Obfuscation is empty
+	if Obfuscation == "" {
+		// if fileSizeFlag is true
+		if fileSizeFlag {
+			fmt.Printf("[!] The size of the encrypted shellcode exceeds the maximum display limit.\n\n[!] Supernova cannot display it on the screen.\n\n")
+		} else {
+			// Print encrypted template
+			fmt.Printf("[+] The encrypted payload with %s:\n\n%s\n\n", strings.ToUpper(Encryption), template)
+		}
+	}
+
+	return template, encryptedShellcode
+}
+
+// OutputManager function
+func OutputManager(OutFile string, Language string, template string) {
+	// Outfile option is enable
+	if OutFile != "" {
+		language := strings.ToLower(Language)
+		if language == "raw" {
+			err := Output.SaveShellcodeToFile(template, OutFile)
+			if err != nil {
+				fmt.Println("[!] Error:", err)
+				return
+			}
+		} else {
+			err := Output.SaveOutputToFile(template, OutFile)
+			if err != nil {
+				fmt.Println("[!] Error:", err)
+				return
+			}
+		}
+	}
+}
